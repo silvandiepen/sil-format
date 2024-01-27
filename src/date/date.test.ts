345d76cd346038c793ad379b97fcc1f6834dc9ff
Sil van Diepen <me@sil.mt>
2024-01-27 09:45:58 +0100

    export multiple types, test main getDate

--- src/date/date.test.ts
+++ src/date/date.test.ts
@@ -2,6 +2,45 @@ import { getDate } from ".";
 import { Language } from "../types";
 
 describe("test", () => {
+  it("Should return the date from the date now",()=>{
+
+    const date = new Date();
+    const result = getDate(date);
+    expect(result).toBeTruthy();
+    expect(result).toHaveProperty('day');
+    expect(result.day).toHaveReturnedWith(expect.any(Number));
+    expect(result).toHaveProperty('month');
+    expect(result.month).toHaveReturnedWith(expect.any(Number));
+    expect(result).toHaveProperty('month_index');
+    expect(result.month_index).toHaveReturnedWith(expect.any(Number));
+    expect(result).toHaveProperty('month_name');
+    expect(result.month_name).toHaveReturnedWith(expect.any(String));
+    expect(result).toHaveProperty('month_abbr');
+    expect(result.month_abbr).toHaveReturnedWith(expect.any(String));
+    expect(result).toHaveProperty('weekday');    
+    expect(result.weekday).toHaveReturnedWith(expect.any(String));
+    expect(result).toHaveProperty('weekday_name');
+    expect(result.weekday_name).toHaveReturnedWith(expect.any(String));
+
+    expect(result).toHaveProperty('weekday_abbr');
+    expect(result.weekday_abbr).toHaveReturnedWith(expect.any(String));
+
+    expect(result).toHaveProperty('year');
+    expect(result.year).toHaveReturnedWith(expect.any(Number));
+
+    expect(result).toHaveProperty('hours');
+    expect(result.hours).toHaveReturnedWith(expect.any(Number));
+
+    expect(result).toHaveProperty('minutes');
+
+    expect(result.minutes).toHaveReturnedWith(expect.any(Number));
+
+    expect(result).toHaveProperty('seconds');
+    expect(result.seconds).toHaveReturnedWith(expect.any(Number));
+
+  
+
+  });
   it("Should return the right formatted date object", () => {
     const date = new Date("2023 05 04");
     expect(getDate(date)).toEqual({
